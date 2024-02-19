6556a67dc77bff182dc0cdae5323288b5e135c5e
Sujan Gyawali <gyasujan@gmail.com>
2024-02-19 20:09:52 +0545

    URL building using redirect and url_for function base on the condition match

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 
 app=Flask(__name__)
 
-@app.route('/blog/<int:postID>')
-def show_blog(postID):
-    return f"Blog number {postID}!"
+#variable rule
+# @app.route('/blog/<int:postID>')
+# def show_blog(postID):
+#     return f"Blog number {postID}!"
+
+#redirecting url
+@app.route("/admin")
+def hello_admin():#view function
+    return f"Hello admin !"
+
+
+@app.route("/guest/<guest>")
+def hello_guest(guest):
+    return f"Hello {guest} !"
+
+
+@app.route('/user/<user>')
+def hello_user(user):
+    if user=='admin':
+        return redirect(url_for('hello_admin'))#view function name
+    else:
+        return redirect(url_for("hello_guest",guest=user))
     
+
 if __name__ == '__main__':
     app.run(debug=True)
