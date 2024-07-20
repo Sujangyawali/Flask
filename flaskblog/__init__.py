971daa73c3fb5f6676a1af29dd31726bf4944c11
Sujan Gyawali <gyasujan@gmail.com>
2024-07-20 17:29:27 +0545

    Incorporation login resitation, conting routes based on the login and created Account Route for user profile

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -10,6 +10,9 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 db=SQLAlchemy(app) # SQLAlchemy database instance
 bcrypt=Bcrypt(app)
 login_manager=LoginManager(app)
+login_manager.login_view='login' # Redirect to 'login' view if not authenticated
+login_manager.login_message_category='info'
+
 
 app.app_context().push()
  
