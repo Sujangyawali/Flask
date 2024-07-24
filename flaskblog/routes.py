28ade69a775535869cc64c8d570ab18e6a0a657c
Sujan Gyawali <gyasujan@gmail.com>
2024-07-24 23:12:00 +0545

    added feature to add post also added route for to move to single post

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,35 +3,15 @@ import os
 from PIL import Image
 from flask import render_template,flash,redirect,url_for,request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User,Post 
 from flask_login import login_user, current_user, logout_user, login_required
 
 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    },
-    {
-        'author': 'Sujan Gywali',
-        'title': 'Blog Post 3',
-        'content': 'Third post content',
-        'date_posted': 'July 7, 2024'
-    }
-]
-
 @app.route("/")
 @app.route("/home")
 def home():
+    posts=Post.query.all()
     return render_template('home.html',posts=posts)
 
 
@@ -90,7 +70,6 @@ def save_pircture(form_picture):
 def account():
     form=UpdateAccountForm()
     if form.validate_on_submit():
-        print(form.picture)
         if form.picture.data:
             picture_file = save_pircture(form.picture.data)
             current_user.image_file=picture_file #get picture file-->save to satic directory-->update file directory to db
@@ -105,3 +84,20 @@ def account():
     image_file=url_for('static',filename='profile_pics/'+current_user.image_file) #current_user.image_file is db User column 
     return render_template('account.html', title='Account'
                            ,image_file=image_file,form=form)
+
+@app.route( "/post/new",methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!','success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)
+
+@app.route( "/post/<int:post_id>",methods=['GET', 'POST'])
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
