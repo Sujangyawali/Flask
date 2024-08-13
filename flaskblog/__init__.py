1a9cf199bf4da11621c6c06b3c325a1b47d71ea2
Sujan Gyawali <gyasujan@gmail.com>
2024-08-13 22:47:12 +0545

    password reset

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy #connecting to DB
 from flask_bcrypt import Bcrypt #pw hashing
 from flask_login import LoginManager #provides user session management
+from flask_mail import Mail #sending mail to user 
 
 app=Flask(__name__)
 
@@ -12,6 +14,13 @@ bcrypt=Bcrypt(app)
 login_manager=LoginManager(app)
 login_manager.login_view='login' # Redirect to 'login' view if not authenticated
 login_manager.login_message_category='info'
+app.config['MAIL-SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL-PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
 
 
 app.app_context().push()
